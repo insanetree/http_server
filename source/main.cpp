44218f52573bd2c1fc0a7393b5951f62bb2f270f
insanetree <pa200059d@student.etf.bg.ac.rs>
2025-01-12 14:56:00 +0100

    Initial http_server creation

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "http_server.h"
+
+using namespace insanetree;
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
+    http_server server{4200, 80};
     return 0;
 }
